a0d1611ff868d580562cb430fa46880bbb48a862
donGog <thomasgoguelin@gmail.com>
2017-02-27 01:24:57 +0100

    parse mesh function

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ module.exports = function (THREE) {
 			var vector = new THREE.Vector3();
 			var normalMatrixWorld = new THREE.Matrix3();
 
-			return function ( scene, args ) {
+			return function ( scene, ...args ) {
 
 				var output = '';
 
@@ -162,12 +162,12 @@ module.exports = function (THREE) {
 
 						// if object is hidden - exit
 						if(object.visible == false) return; 
-						else if (arguments.length > 1 ) {
-							console.log("arg length > 1", arguments)
-							for(var i=1; i<arguments.length; i++){
-								if(object.name == arguments[i]) return; 
+						else if (args.length > 1 ) {
+							console.log("arg length > 1", args)
+							for(var i=1; i<args.length; i++){
+								if(object.name == args[i]) return; 
 								console.log("object name", object.name)
-								console.log("argument value", arguments[i])
+								console.log("argument value", args[i])
 							}
 						}
 
